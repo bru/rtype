0ba0e14471d4bf006e367930bec1794c944bbbdf
Riccardo Cambiassi <riccardo@headshift.com>
2009-10-28 17:44:13 +0000

    Moved template_tag list to model (blog for now)

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,16 @@
 class Blog < ActiveRecord::Base
   include MovableType
   
-  has_many :entries, :foreign_key => 'entry_blog_id'
+  has_many :entries, :foreign_key => 'entry_blog_id', :conditions => { :entry_class => "entry" }
+  has_many :pages, :class_name => 'Entry', :foreign_key => 'entry_blog_id', :conditions => { :entry_class => "page" }
+  has_many :categories
+  has_many :comments
   
+  def self.tag_methods
+    %w(ArchiveUrl id name description language SitePath url )
+  end
+ 
+  # Tag Methods 
   def basename
     blog_site_url.match(/([^\/]+)\/?$/)[0]
   end
@@ -10,6 +18,18 @@ class Blog < ActiveRecord::Base
   def path
     blog_site_url.sub(/^(http:\/\/){0,1}[^\/]*\//, "")
   end
+  alias_method :site_path, :path
+  
+  def entry_count
+    entries.count
+  end
+  def category_count
+    categories.count
+  end
+  def comment_count
+    comments.count
+  end
+  #alias_method :ping_count, :pings_count
   
   def url
     blog_site_url
--- lib/movable_type/template_tags/blog.rb
+++ lib/movable_type/template_tags/blog.rb
@@ -1,6 +1,6 @@
 # Blog tags
 
-%w(ArchiveUrl id name description language SitePath url ).each do |attribute|
+Blog.tag_methods.each do |attribute|
   attribute = attribute.underscore.downcase
   MovableType::TemplateTags::tag 'blog' + attribute do |tag|
    blog = tag.globals.blog
